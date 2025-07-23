f08ea3e9606102c1a6e3b1773880650889440625
Syreli <8gege8@gmail.com>
2025-07-23 19:28:03 +0200

    Print a Report

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from stats import count_characters
+from stats import get_word_count
+from stats import sort_on
+from stats import get_letter_list
 
 def get_book_text(books):
     with open(books) as f:
@@ -9,7 +12,22 @@ def get_book_text(books):
 def main():
     books = "books/frankenstein.txt"
     book_content = get_book_text(books)
-    count_characters(book_content)
+    get_word_count(book_content)
+    character_dict = count_characters(book_content)
+    letter_list = get_letter_list(character_dict)
+    letter_list.sort(reverse=True, key=sort_on)
     
+
+    print("=========== BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt..")
+    print("----------- Word Count ----------")
+    print(f"Found {get_word_count(book_content)} total words")
+    print("--------- Character Count -------")
+    for item in letter_list:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+    
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,8 @@
+def get_word_count(book_content):
+    words = book_content.split()
+    return len(words)
+
+
 def count_characters(book_content):
     letter_count = {}
     for letter in book_content.lower():
@@ -5,4 +10,22 @@ def count_characters(book_content):
             letter_count[letter] += 1
         else:
             letter_count[letter] = 1
-    return print(letter_count)
+    return letter_count
+
+def get_letter_list(letter_count):
+    letter_list = []
+    for key, value in letter_count.items():
+        letter_sort = {"char": key, "num": value}
+        letter_list.append(letter_sort)
+    return letter_list
+
+def sort_on(item):
+    return item["num"]
+
+
+
+        
+    
+    
+
+    
